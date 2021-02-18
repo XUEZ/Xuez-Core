010fe094b24f3ebee317a80542d91f1b79e23454
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-02-18 04:29:49 -0700

    Update XUEZ URLs

--- src/init.cpp
+++ src/init.cpp
@@ -604,7 +604,7 @@ void SetupServerArgs(NodeContext& node)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/XUEZ/Xuez-Core>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
